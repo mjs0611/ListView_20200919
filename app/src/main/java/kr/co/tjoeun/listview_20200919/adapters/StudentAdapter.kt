ba04cfa68545c80c7df460c154ac23d99250e523
JunSeung Mo <mojuns@gmail.com>
2020-09-19 13:59:21 +0900

    학생 어댑터클래스 뼈대작업

---
+++ app/src/main/java/kr/co/tjoeun/listview_20200919/adapters/StudentAdapter.kt
@@ -0,0 +1,30 @@
+package kr.co.tjoeun.listview_20200919.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kr.co.tjoeun.listview_20200919.R
+import kr.co.tjoeun.listview_20200919.datas.Student
+
+class StudentAdapter(
+    val ,mContext: Context,
+    val resID: Int,
+    val mList: ArrayList<Student>) : ArrayAdapter<Student>(mContext, resID, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+
+}
